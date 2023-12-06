bd1d612a695210625f903f879012ffb98a9c8ebf
Denis Koinash <d.koinash@cyclone-nov.ru>
2023-12-06 17:50:38 +0300

    Добавлен цвет точек, формирование вектора точек и смена цвета точек выделены в отдельные функции, запускаемые в отдельных потоках

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <mutex>
 #include <random> // std::uniform_int_distribution
 #include <thread>
 #include <vector>
@@ -19,7 +20,6 @@ static const uint32_t WIN_HEIGHT = 1008;
 static const uint32_t FPS_LIMIT  = 60;
 
 static const sf::Color BACKGROUND_CLR = sf::Color(1, 1, 1);
-static const sf::Color GRID_COLOR     = sf::Color(255, 255, 255, 64);
 
 struct Point
 {
@@ -32,12 +32,62 @@ struct Point
     : x(x_)
     , y(y_)
   {}
+  void setColor(uint8_t _red = 0, uint8_t _green = 0, uint8_t _blue = 0)
+  {
+    red   = _red;
+    green = _green;
+    blue  = _blue;
+  }
 
   uint32_t x = 0;
   uint32_t y = 0;
+  uint8_t  red;
+  uint8_t  green;
+  uint8_t  blue;
 };
 
 std::vector<Point> field;
+std::mutex         lockField;
+
+auto
+randomizer(int minValue, int maxValue)
+{
+  std::random_device              rd;
+  std::mt19937                    gen(rd());
+  std::uniform_int_distribution<> distrib(minValue, maxValue);
+
+  return distrib(gen);
+}
+
+void
+changeColor()
+{
+  int pos = 0;
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
+    lockField.lock();
+    field[pos].setColor(randomizer(0, 255), randomizer(0, 255), randomizer(0, 255));
+    lockField.unlock();
+    if (pos >= field.size())
+      pos = 0;
+    else
+      ++pos;
+  }
+}
+
+void
+fillingField()
+{
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    lockField.lock();
+
+    field.push_back(Point(randomizer(0, WIN_WIDTH), randomizer(0, WIN_HEIGHT)));
+
+    lockField.unlock();
+  }
+}
 
 ENTRYPOINT_MAIN
 {
@@ -47,8 +97,14 @@ ENTRYPOINT_MAIN
   sf::Font font;
   if (!font.loadFromFile("Poiret One.ttf")) return false;
 
+  std::thread th_field(fillingField);
+  std::thread th_color(changeColor);
+  th_field.detach();
+  th_color.detach();
+
   while (window.isOpen()) {
     sf::Event event;
+#if DEPRECATED
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
@@ -81,6 +137,9 @@ ENTRYPOINT_MAIN
     // point.y = rndHeight();
 
     field.push_back(Point(rndWidth(), rndHeight()));
+#else
+
+#endif // DEPRECATED
 
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
@@ -100,8 +159,11 @@ ENTRYPOINT_MAIN
 
     for (int i = 0; i < field.size(); ++i) {
       sf::CircleShape circle(1.0f);
+      lockField.lock();
       circle.move(field[i].x, field[i].y);
-      circle.setFillColor(sf::Color(128, 128, 128));
+
+      circle.setFillColor(sf::Color(field[i].red, field[i].green, field[i].blue));
+      lockField.unlock();
       window.draw(circle);
     }
 

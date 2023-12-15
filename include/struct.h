9e0cb2ca8b6289dfe5553462263a27fd83c24c8c
Denis Koinash <d.koinash@cyclone-nov.ru>
2023-12-15 13:06:06 +0300

    Падающий элемент

---
+++ include/struct.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct Point{
+    float x = 0;
+    float y = 0;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include <vector>
 
+#include <struct.h>
+
 #define PI std::acos(-1)
 
 #include <SFML/Graphics.hpp>
@@ -22,123 +24,42 @@ static const uint32_t WIN_WIDTH     = 1920;
 static const uint32_t WIN_HEIGHT    = 1080;
 static const uint32_t FPS_LIMIT     = 60;
 static const uint32_t MAX_DOT_COUNT = 800;
+static const uint32_t STEP_TIME     = 1; // мс
+static const float    BALL_RADIUS   = 3.99;
 static const float    STEP_ANGLE    = 0.02f;
 static const float    MIN_SIZE      = 0.6f;
 static const float    MAX_SIZE      = 1.8f;
 
-static const sf::Color BACKGROUND_CLR = sf::Color(1, 1, 1);
-
-struct Point
-{
-  Point()
-  {
-    x     = 0.f;
-    y     = 0.f;
-    r     = 0.f;
-    red   = 255;
-    green = 255;
-    blue  = 255;
-  }
-  Point(float   x_,
-        float   y_,
-        float   r_     = 1.f,
-        uint8_t red_   = 255,
-        uint8_t green_ = 255,
-        uint8_t blue_  = 255)
-    : x(x_)
-    , y(y_)
-    , r(r_)
-    , red(red_)
-    , green(green_)
-    , blue(blue_)
-  {}
-  void setColor(uint8_t _red, uint8_t _green, uint8_t _blue)
-  {
-    red   = _red;
-    green = _green;
-    blue  = _blue;
-  }
-
-  float   x;
-  float   y;
-  float   r;
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
-};
-
-std::vector<Point> field;
-std::mutex         lockField;
-
-template<class T>
-T
-randomizer(T minValue, T maxValue)
-{
-  std::random_device               rd;
-  std::mt19937                     gen(rd());
-  std::uniform_real_distribution<> distrib(minValue, maxValue);
-
-  return distrib(gen);
-}
+Point      m_point;
+std::mutex lockPoint;
 
 void
-changeColor()
+fallenPoint()
 {
-  int pos = 0;
+  bool   isFall = true;
+  double timer  = 0;
   while (true) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    lockField.lock();
-    field[pos].setColor(randomizer(0, 255), randomizer(0, 255), randomizer(0, 255));
-    lockField.unlock();
-    if (pos >= field.size())
-      pos = 0;
-    else
-      ++pos;
-  }
-}
-
-void
-rotate(Point base = Point(0.f, 0.f), float angle = 0.f)
-{
-  while (true) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    float radAngle = angle / 180 * PI;
-
-    lockField.lock();
-    for (auto it = field.begin(); it != field.end(); ++it) {
-      Point pnt = *it;
-
-      float loc_x = pnt.x - base.x;
-      float loc_y = pnt.y - base.y;
-      float r     = std::sqrtf(std::powf(loc_x, 2.f) + std::powf(loc_y, 2.f));
-      float alfa  = std::acosf(loc_x / r);
-      pnt.x       = r * std::cosf(alfa + radAngle);
-      pnt.y       = std::sqrtf(std::powf(r, 2.f) - std::powf(pnt.x, 2.f));
-      pnt.x       = pnt.x + base.x;
-      pnt.y       = pnt.y + base.y;
-
-      *it = pnt;
+    if (isFall) { // Падение
+      lockPoint.lock();
+      m_point.y += m_point.y * std::powf(timer, 2) / 2.f;
+      if (m_point.y >= WIN_HEIGHT - BALL_RADIUS) {
+        isFall = false;
+        timer  = 0;
+      };
+      lockPoint.unlock();
+    } else { // Отскок
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+      isFall    = true;
+      m_point.x = WIN_WIDTH / 2;
+      m_point.y = 200;
+      // timer  = 0;
     }
-    lockField.unlock();
+    timer += 0.001 * STEP_TIME;
+    std::this_thread::sleep_for(std::chrono::milliseconds(STEP_TIME));
   }
 }
 
-void
-fillingField()
-{
-  while (true) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    lockField.lock();
-
-    field.push_back(Point(randomizer(-float(WIN_WIDTH), float(WIN_WIDTH)),
-                          randomizer(-float(WIN_HEIGHT), float(WIN_HEIGHT)),
-                          randomizer(MIN_SIZE, MAX_SIZE)));
-    if (field.size() > MAX_DOT_COUNT) field.erase(field.begin());
-
-    lockField.unlock();
-  }
-}
+static const sf::Color BACKGROUND_CLR = sf::Color(1, 1, 1);
 
 ENTRYPOINT_MAIN
 {
@@ -150,13 +71,19 @@ ENTRYPOINT_MAIN
   sf::Font font;
   if (!font.loadFromFile("Poiret One.ttf")) return false;
 
-  std::thread th_field(fillingField);
-  std::thread th_color(changeColor);
-  Point       base;//(WIN_WIDTH / 2, WIN_HEIGHT / 2);
-  std::thread th_rotation(rotate, base, STEP_ANGLE);
-  th_field.detach();
-  th_color.detach();
-  th_rotation.detach();
+  // std::thread th_field(fillingField);
+  // std::thread th_color(changeColor);
+  // Point       base;//(WIN_WIDTH / 2, WIN_HEIGHT / 2);
+  // std::thread th_rotation(rotate, base, STEP_ANGLE);
+  // th_field.detach();
+  // th_color.detach();
+  // th_rotation.detach();
+
+  m_point.x = WIN_WIDTH / 2;
+  m_point.y = 200;
+
+  std::thread th_fallenPoint(fallenPoint);
+  th_fallenPoint.detach();
 
   while (window.isOpen()) {
     sf::Event event;
@@ -177,17 +104,23 @@ ENTRYPOINT_MAIN
 
     window.clear(BACKGROUND_CLR);
 
-    for (int i = 0; i < field.size(); ++i) {
-      lockField.lock();
-      sf::CircleShape circle(field[i].r);
-      if (field[i].x > 0.f && field[i].y > 0.f && field[i].x < float(WIN_WIDTH) &&
-          field[i].y < float(WIN_HEIGHT))
-        circle.move(field[i].x, field[i].y);
+    sf::CircleShape circle(BALL_RADIUS);
+    lockPoint.lock();
+    circle.move(m_point.x, m_point.y);
+    lockPoint.unlock();
+    circle.setFillColor(sf::Color(255, 255, 255));
+    window.draw(circle);
+    // for (int i = 0; i < field.size(); ++i) {
+    //   lockField.lock();
+    //   sf::CircleShape circle(field[i].r);
+    //   if (field[i].x > 0.f && field[i].y > 0.f && field[i].x < float(WIN_WIDTH) &&
+    //       field[i].y < float(WIN_HEIGHT))
+    //     circle.move(field[i].x, field[i].y);
 
-      circle.setFillColor(sf::Color(field[i].red, field[i].green, field[i].blue));
-      lockField.unlock();
-      window.draw(circle);
-    }
+    //   circle.setFillColor(sf::Color(field[i].red, field[i].green, field[i].blue));
+    //   lockField.unlock();
+    //   window.draw(circle);
+    // }
 
     window.display();
   }
